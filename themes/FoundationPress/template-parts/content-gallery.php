903a307a97625822dfeacc822659b53037e4d825
Riley MacDonald <riley@Rileys-MacBook-Air.local>
2017-12-11 16:26:05 -0500

    started trying to add taxonomy to class of images in gallery

--- themes/FoundationPress/template-parts/content-gallery.php
+++ themes/FoundationPress/template-parts/content-gallery.php
@@ -1,3 +1,4 @@
-<!--<div class="<?php// get_the_terms(get_the_ID(), "taxonomy") ?>">
-</div>-->
+<div class="<?php get_the_terms(get_the_ID(), 'mascot-type') ?>">
+
 <?php  the_post_thumbnail() ?>
+</div>
