681f66e84fb6fa06c09de7eb7eeece920994730f
Sam C. Finan <samcfinan@gmail.com>
2017-12-19 14:15:31 -0500

    Our process images fixed

--- themes/FoundationPress/page-our-process.php
+++ themes/FoundationPress/page-our-process.php
@@ -14,21 +14,29 @@
 
             <p class="our-process-description"> 2. After you sign the quote and make a deposit to become a client, we begin to build your mascot. </p>
 
-            <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process1_Headcarve@2x.png'?>" /> 
+            <div class="our-process-img-container">
+                <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process1_Headcarve@2x.png'?>" /> 
+            </div>
 
             <p class="our-process-description"> 3. Once you approve the foundation pieces, we add the fabric. </p>
 
-            <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process2_Head@2x.png'?>" /> 
+            <div class="our-process-img-container">
+                <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process2_Head@2x.png'?>" /> 
+            </div>
 
             <p class="our-process-description"> 4. The fabric and details are then added. Details such as eyebrows and logos will be tacked, not sewn, in place until final approval is received. </p> 
 
-            <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process3_HeadCover@2x.png'?>" /> 
-
-            <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process4_Claws@2x.png'?>" /> 
-
+            <div class="our-process-img-container">
+                <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process3_HeadCover@2x.png'?>" /> 
+            </div>
+            <div class="our-process-img-container">
+                <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process4_Claws@2x.png'?>" /> 
+            </div>
             <p class="our-process-description"> 5. You make the final payment and we ship the mascot. The tracking number, laundry and dressing instructions are e-mailed to you. </p>
 
-            <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process5_Final@2x.png'?>" />
+            <div class="our-process-img-container">
+                <img class="our-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process5_Final@2x.png'?>" />
+            </div>
 
             <p class="our-process-description"> 6. We follow up with you, to know how you and your mascot are doing! </p> 
 
@@ -38,12 +46,22 @@
 
              </div> 
             <div class="desktop-process-container"> 
-                <div class="desktop-process-img-div"> 
-                    <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process1_Headcarve@2x.png'?>" /> 
-                    <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process2_Head@2x.png'?>" /> 
-                    <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process3_HeadCover@2x.png'?>" /> 
-                    <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process4_Claws@2x.png'?>" /> 
-                    <img class="desktop-process-img-last" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process5_Final@2x.png'?>" />
+                <div class="desktop-process-img-div">
+                    <div class="our-process-desktop-img-container">
+                        <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process1_Headcarve@2x.png'?>" /> 
+                    </div>
+                    <div class="our-process-desktop-img-container">
+                        <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process2_Head@2x.png'?>" /> 
+                    </div>
+                    <div class="our-process-desktop-img-container">
+                        <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process3_HeadCover@2x.png'?>" /> 
+                    </div>
+                    <div class="our-process-desktop-img-container">
+                        <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process4_Claws@2x.png'?>" /> 
+                    </div>
+                    <div class="our-process-desktop-img-last">
+                        <img class="desktop-process-img" src="<?php echo get_template_directory_uri() . '/src/assets/images/About-Page/Our-Process/Process5_Final@2x.png'?>" />
+                    </div>
                 </div>
                 <div class="desktop-process-description-div"> 
                     <p class="desktop-process-description-first">  1. You tell us what you want. We sketch it and send a detailed quote to you. </p>
