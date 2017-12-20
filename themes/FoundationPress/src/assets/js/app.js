4dbcd39496d5bc28f0850f5556c190656fe4cf8a
Max Page <maxpage@Maxs-MacBook-Pro.local>
2017-12-20 15:45:27 -0500

    Repaired build a quote files

--- themes/FoundationPress/src/assets/js/app.js
+++ themes/FoundationPress/src/assets/js/app.js
@@ -47,25 +47,25 @@ function toggleFilter() {
   if (window.innerWidth > 1023){
   $('.gallery-filter-list-container').toggle();
   } else {
-  $('.gallery-filter-list-container').toggle();
+  $('.gallery-filter-list-container').toggle();    
   $('.gallery-forum').toggle();
   }}
 
 
 
-$('.build-quote-next-one').on('click' , function() {
+$('.build-quote-next').on('click' , function() {
   $('.page-one').hide();
-  $('.mascot-type').css( {'display': 'block'} );
+  $('.mascot-type').css( {display: 'block'} );
 
 });
 
 $('.build-quote-next-mascot-type').on('click', function() {
   $('.mascot-type').hide();
   $('.mascot-head-type').css("display", "flex");
-});
+}); 
 
 $('.build-quote-back-head-type').on('click' , function() {
-  $('.mascot-head-type').hide();
+  $('.mascot-head-type').hide(); 
   $('.mascot-type').css("display", "block");
 });
 
@@ -73,7 +73,6 @@ $('.build-quote-next-head-type').on('click', function() {
   $('.mascot-head-type').hide();
   $('.mascot-body-type').css("display", "flex");
 });
-
 
 $('.build-quote-next-body-type').on('click', function () {
   $('.mascot-body-type').hide();
@@ -96,66 +95,7 @@ $('#levelthree').on('click', function() {
    $('.mascot-detail-type-display-lashes').css("display" , "none");
   $('.mascot-detail-type-display-glasses').css("display" , "flex");
   $('.mascot-detail-type-display').css("display" , "none");
-}); 
-
-$('.build-quote-back-body-type').on('click', function() {
-  $('.mascot-body-type').hide();
-  $('.mascot').css('display', 'block');
 });
 
-$('.build-quote-next-body-type').on('click', function() {
-  $('.mascot-body-type').hide();
-  $('.wpcf7-form').css('display', 'block');
-});
 
-// Filling out Contact Form 7 checkboxes depending on which element is clicked
-$('.mascot-container-pizza').on('click', function() {
-  $('input:radio[value=uni-body]:nth(0)').attr('checked' , true);
-  $('.mascot-container-pizza').css('border', '2px solid #62c7c9');
-});
 
-$('.mascot-container-bear').on('click', function() {
-  $('input:radio[value=head-body]:nth(0)').attr('checked' , true);
-  $('.mascot-container-bear').css('border', '2px solid #62c7c9');
-});
-
-$('#human-head').on('click', function() {
-  $('input:radio[value=humanhead]:nth(0)').attr('checked' , true);
-  $('#human-head').css('border', '2px solid #62c7c9');
-});
-
-$('#animal-head').on('click', function() {
-  $('input:radio[value=animalhead]:nth(0)').attr('checked' , true);
-
-  $('#animal-head').css('border', '2px solid #62c7c9');
-});
-
-$('#monster-head').on('click', function() {
-  $('input:radio[value=monsterhead]:nth(0)').attr('checked' , true);
-  $('#monster-head').css('border', '2px solid #62c7c9');
-});
-
-$('#body').on('click', function() {
-  $('input:radio[value=Body]:nth(0)').attr('checked' , true);
-  $('#body').css('border', '2px solid #62c7c9');
-});
-
-$('#clothes').on('click', function() {
-  $('input:radio[value=Clothes]:nth(0)').attr('checked' , true);
-  $('#clothes').css('border', '2px solid #62c7c9');
-});
-
-$('#level-one').on('click', function() {
-  $('input:radio[value=levelone]:nth(0)').attr('checked' , true);
-  $('#level-one').css('border', '2px solid #62c7c9');
-});
-
-$('#level-two').on('click', function() {
-  $('input:radio[value=leveltwo]:nth(0)').attr('checked' , true);
-  $('#level-two').css('border', '2px solid #62c7c9');
-});
-
-$('#level-three').on('click', function() {
-  $('input:radio[value=levelthree]:nth(0)').attr('checked' , true);
-  $('#level-three').css('border', '2px solid #62c7c9');
-});
